2e155058c5a578111fa78e5404cf2979ac8850ec
Vasu-Patel_007 <patelvasu711@gmail.com>
2022-12-08 22:38:01 -0500

    typo in the code that prevented from deleting the product

--- testing/delete_product.py
+++ testing/delete_product.py
@@ -2,10 +2,11 @@ import pymongo
 
 shoename = "sucks"
 
-client = pymongo.MongoClient("mongodb+srv://admin:patel@sikewearcluster.r7rsoch.mongodb.net/?retryWrites=true&w=majority")
-data = client.test.shoe
 
-data.delete_one({"shoeName":shoename})
+client = pymongo.MongoClient("mongodb+srv://admin:patel@sikewearcluster.r7rsoch.mongodb.net/?retryWrites=true&w=majority")
+data = client.test.shoes
+
+data.delete_one({"shoeName":"sucks"})
 entry = data.find_one({"shoeName":shoename})
 try:
     if( not entry):
